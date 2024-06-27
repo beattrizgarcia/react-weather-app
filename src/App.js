1cf3086a791ef913f65fab8d1bd197a271af1bb2
beattrizgarcia <159271671+beattrizgarcia@users.noreply.github.com>
2024-06-27 11:16:01 +0100

    Added the github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,38 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by
+        <a
+          href="https://github.com/beattrizgarcia"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Beatriz Garcia
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/beattrizgarcia/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://capable-toffee-4bbba8.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Netlify
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
