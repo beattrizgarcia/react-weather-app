f8f9dee05bcd20ed25369bea9e2daee21e86ecaf
beattrizgarcia <159271671+beattrizgarcia@users.noreply.github.com>
2024-07-01 11:36:25 +0100

    Added the basic HTML and styling

--- src/App.js
+++ src/App.js
@@ -1,38 +1,42 @@
 import "./App.css";
+import Weather from "./Weather.js";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by
-        <a
-          href="https://github.com/beattrizgarcia"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Beatriz Garcia
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/beattrizgarcia/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Github
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://capable-toffee-4bbba8.netlify.app/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          Netlify
-        </a>
-        .
-      </footer>
+      <div className="container">
+        
+        <Weather />
+        <footer>
+          This project was coded by
+          <a
+            href="https://github.com/beattrizgarcia"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Beatriz Garcia
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/beattrizgarcia/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>{" "}
+          and hosted on{" "}
+          <a
+            href="https://capable-toffee-4bbba8.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Netlify
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
